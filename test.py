3c236ff4c45efd05e6b811f538b7e2094a249791
Djordje Petrović <djordje.pe.petrovic@gmail.com>
2024-12-03 19:44:24 +0100

    test: send html emails

--- test.py
+++ test.py
@@ -6,4 +6,12 @@ with open("settings.toml", "rb") as f:
 email = data["email"]
 
 mail = GmailSend(email["email"], email["password"])
+
+text = "This is your polymensa menu for the day:\n\nBreakfast: {}\nLunch: {}\nDinner: {}".format("hj", "hj", "hjk")
+with open("src/email.html", "r") as f:
+    html = f.read()
+
+mail.send_html(["planet.kepler.69c@gmail.com"], "test", text, html)
+
+mail.send(["planet.kepler.69c@gmail.com"], "test")
 mail.send(["planet.kepler.69c@gmail.com"], "test")
