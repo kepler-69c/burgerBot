fb392f0f1f1ebb653949ba396fe5e4223ab9c35c
Djordje Petrović <djordje.pe.petrovic@gmail.com>
2024-12-05 09:40:22 +0100

    fix: move email key from .toml to .env file

--- email_test.py
+++ email_test.py
@@ -1,14 +1,25 @@
+import os
 import json
-import tomllib
 from helpers.fetch import Polymensa
 from helpers.sendmail import BurgerSend
 
-with open("settings.toml", "rb") as f:
-    data = tomllib.load(f)
-api = data["api"]
+email = os.getenv("EMAIL")
+password = os.getenv("PASSWORD")
+
+api = {
+    # id which indicates which eth service is requesting the data
+    client_id: "ethz-wcms", # TODO: am I allowed to use this id?
+    # language; possible: ["en", "de"]
+    lang: "en",
+    # idk
+    rsfirst: 0,
+    rssize: 1,
+    # facility number
+    facility: 9,
+}
 
 mensa = Polymensa(**api)
 meals = mensa.get_dishes()
 
-mail = BurgerSend(data["email"]["email"], data["email"]["password"])
+mail = BurgerSend(email, password)
 mail.send(["sendto@gmail.com"], meals)
