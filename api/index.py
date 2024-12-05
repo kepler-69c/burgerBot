dc037211cbc286a82c437958de76e9a7f39ffd49
Djordje Petrović <djordje.pe.petrovic@gmail.com>
2024-12-05 20:03:50 +0100

    fix: env variables in vercel

--- api/index.py
+++ api/index.py
@@ -1,22 +1,25 @@
 from flask import Flask, jsonify
 import os
 import json
+from dotenv import load_dotenv
 from helpers.fetch import Polymensa
 from helpers.sendmail import BurgerSend
 
+load_dotenv()
 app = Flask(__name__)
+
 
 @app.route("/")
 @app.route("/api")
 def send_email():
     try:
-        email = os.getenv("EMAIL")
-        password = os.getenv("PASSWORD")
-        recipients = os.getenv("RECIPIENTS").split(",")
+        email = os.environ.get("EMAIL")
+        password = os.environ.get("PASSWORD")
+        recipients = os.environ.get("RECIPIENTS").split(",")
 
         api = {
             # id which indicates which eth service is requesting the data
-            client_id: "ethz-wcms", # TODO: am I allowed to use this id?
+            client_id: "ethz-wcms",  # TODO: am I allowed to use this id?
             # language; possible: ["en", "de"]
             lang: "en",
             # idk
