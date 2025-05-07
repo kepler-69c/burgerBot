01045d0ac5605424dec43904793bfd5b40836fe6
Djordje Petrović <djordje.pe.petrovic@gmail.com>
2025-05-07 12:38:06 +0200

    fix(tests): remove test emails after running

--- test/db_test.py
+++ test/db_test.py
@@ -7,15 +7,18 @@ class DBTest(unittest.TestCase):
         token = add_email("mail@example.com", "always")
         self.assertIsNotNone(token)
         self.assertEqual(get_email(token)["email"], "mail@example.com")
+        delete_email(token)
 
     def test_update_settings(self):
         token = add_email("mail@example.com", "always")
         update_settings(token, "never")
         self.assertEqual(get_email(token)["sending"], "never")
+        delete_email(token)
 
     def test_get_emails(self):
-        add_email("mail@example.com", "always")
+        token = add_email("mail@example.com", "always")
         self.assertGreaterEqual(len(get_emails()), 1)
+        delete_email(token)
 
     def test_delete_email(self):
         token = add_email("mail@example.com", "always")
